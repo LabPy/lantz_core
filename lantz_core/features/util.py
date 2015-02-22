0670ea70889eea2a93c3189a4597092e147bade7
Matthieu Dartiailh <marul@laposte.net>
2015-02-22 21:32:47 +0100

    Fill in features/util.py module

--- lantz_core/features/util.py
+++ lantz_core/features/util.py
@@ -11,88 +11,216 @@
 """
 from __future__ import (division, unicode_literals, print_function,
                         absolute_import)
-from collections import OrderedDict
 
+
+# --- Methods composers -------------------------------------------------------
 
 class MethodComposer(object):
-    """
-    """
-    __slots__ = ('methods', '_iter')
+    """Function like object used to compose feature methods calls.
 
-    def __init__(self, ):
-        self.methods = OrderedDict()
+    All methods to call are kept in an ordered dict ensuring that they will
+    be called in the right order while allowing fancy insertion based on method
+    id.
+
+    """
+    __slots__ = ('_names', '_methods')
+
+    def __init__(self):
+        self._methods = []
+        self._names = []
 
     def prepend(self, name, method):
+        """Prepend a method to existing ones.
+
+        Parameters
+        ----------
+        name : unicode
+            Id of the method. Used to find it when performing more complex
+            operations on the list of methods.
+        method : MethodType
+            Method bound to a feature which will be called when this object
+            will be called.
+
         """
-        """
-        pass
+        self._names.insert(0, name)
+        self._methods.insert(0, method)
 
     def append(self, name, method):
+        """Append a method to existing ones.
+
+        Parameters
+        ----------
+        name : unicode
+            Id of the method. Used to find it when performing more complex
+            operations on the list of methods.
+        method : MethodType
+            Method bound to a feature which will be called when this object
+            will be called.
+
         """
-        """
-        pass
+        self._names.append(name)
+        self._methods.append(method)
 
     def add_after(self, anchor, name, method):
+        """Add the given method after a given one.
+
+        Parameters
+        ----------
+        anchor : unicode
+            Id of the method after which to insert the given one.
+        name : unicode
+            Id of the method. Used to find it when performing more complex
+            operations on the list of methods.
+        method : MethodType
+            Method bound to a feature which will be called when this object
+            will be called.
+
         """
-        """
-        pass
+        i = self._names.index(anchor)
+        self._names.insert(i+1, name)
+        self._methods.insert(i+1, method)
 
     def add_before(self, anchor, name, method):
+        """Add the given method before the specified one.
+
+        Parameters
+        ----------
+        anchor : unicode
+            Id of the method before which to insert the given one.
+        name : unicode
+            Id of the method. Used to find it when performing more complex
+            operations on the list of methods.
+        method : MethodType
+            Method bound to a feature which will be called when this object
+            will be called.
+
         """
-        """
-        pass
+        i = self._names.index(anchor)
+        self._names.insert(i, name)
+        self._methods.insert(i, method)
 
     def replace(self, name, method):
+        """Replace an existing method by a new one.
+
+        Parameters
+        ----------
+        name : unicode
+            Id of the method of the method to replace.
+        method : MethodType
+            Method bound to a feature which will be called when this object
+            will be called.
+
         """
+        i = self._names.index(name)
+        self._names[i] = name
+        self._methods[i] = method
+
+    def remove(self, name):
+        """Remove a method.
+
+        Parameters
+        ----------
+        name : unicode
+            Id of the method to remove.
+
         """
-        pass
+        i = self._names.index(name)
+        del self._names[i]
+        del self._methods[i]
 
     def reset(self):
-        """
-        """
-        pass
+        """Empty the composer.
 
-    def _rebuild(self):
         """
-        """
-        self._iter = self.methods.values()
+        self._names = []
+        self._methods = []
 
 
-def PreGetComposer(MethodComposer):
+class PreGetComposer(MethodComposer):
+    """Composer used for pre_get methods.
+
     """
-    """
+
     def __call__(self, driver):
+        """Call mimicking a pre_get method and calling all assigned methods
+        in order with the driver as only argument.
+
         """
-        """
-        for m in self._iter:
+        for m in self._methods:
             m(driver)
 
 
-def PostGetComposer(MethodComposer):
+class PostGetComposer(MethodComposer):
+    """Composer for post_get methods.
+
     """
-    """
+
     def __call__(self, driver, value):
+        """Call mimicking a post_get method and calling all assigned methods
+        in order. The value returned by each method is passed to the next one.
+
         """
-        """
-        for m in self._iter:
+        for m in self._methods:
             value = m(driver, value)
         return value
 
 
-def PreSetComposer(MethodComposer):
+class PreSetComposer(MethodComposer):
+    """Composer for pre_set methods.
+
     """
-    """
+
     def __call__(self, driver, value):
+        """Call mimicking a pre_set method and calling all assigned methods
+        in order. The value returned by each method is passed to the next one.
+
         """
-        """
-        for m in self._iter:
+        for m in self._methods:
             value = m(driver, value)
         return m
 
 
-def PostSetComposer(MethodComposer):
+class PostSetComposer(MethodComposer):
+    """Composer for post_set methods.
+
     """
-    """
+
     def __call__(self, driver, value, d_value, response):
-        for m in self._iter:
+        """Call mimicking a post_set method and calling all assigned methods
+        in order.
+
+        """
+        for m in self._methods:
             value = m(driver, value, d_value, response)
+
+
+# --- Customisation decorators ------------------------------------------------
+
+def append(function):
+    """
+    """
+    pass
+
+
+def prepend(function):
+    """
+    """
+    pass
+
+
+def add_after(name):
+    """
+    """
+    pass
+
+
+def add_before(name):
+    """
+    """
+    pass
+
+
+def replace(name):
+    """
+    """
+    pass
