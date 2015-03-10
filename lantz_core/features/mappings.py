9686db3a4501d188ba7d7a92a37adb4463b7c7a1
Matthieu Dartiailh <marul@laposte.net>
2015-03-10 15:30:34 +0100

    Add support for mapping Features.

---
+++ lantz_core/features/mappings.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+    lantz_core.features.mappings
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Feature for values requiring a mapping between user and instrs values.
+
+    :copyright: 2015 by Lantz Authors, see AUTHORS for more details.
+    :license: BSD, see LICENSE for more details.
+
+"""
+from __future__ import (division, unicode_literals, print_function,
+                        absolute_import)
+
+from .feature import Feature
+
+
+class Mapping(Feature):
+    """ Feature using a dict to map user input to instrument and back.
+
+    Parameters
+    ----------
+    mapping : dict
+        Mapping between the user values and instrument values.
+
+    """
+    def __init__(self, getter=None, setter=None, mapping=None, get_format='',
+                 retries=0, checks=None, discard=None):
+        super(Mapping, self).__init__(getter, setter, get_format, retries,
+                                      checks, discard)
+
+        mapping = mapping if mapping else {}
+        self._map = mapping
+        self._imap = {v: k for k, v in mapping.items()}
+        self.creation_kwargs['mapping'] = mapping
+
+        self.modify_behavior('post_get', self.reverse_map_value,
+                             ('reverse_map', 'append'), True)
+
+        self.modify_behavior('pre_set', self.map_value,
+                             ('map', 'append'), True)
+
+    def reverse_map_value(self, instance, value):
+        return self._imap[value]
+
+    def map_value(self, instance, value):
+        return self._map[value]
+
+
+class Bool(Mapping):
+    """ Boolean property.
+
+    True/False are mapped to the mapping values, aliases can also be declared
+    to accept non-boolean values.
+
+    Parameters
+    ----------
+    aliases : dict, optional
+        Keys should be True and False and values the list of aliases.
+
+    """
+    def __init__(self, getter=None, setter=None, mapping=None, aliases=None,
+                 get_format='', retries=0, checks=None, discard=None, ):
+        super(Bool, self).__init__(getter, setter, mapping, get_format,
+                                   retries, checks, discard)
+
+        self._aliases = {True: True, False: False}
+        if aliases:
+            for k in aliases:
+                for v in aliases[k]:
+                    self._aliases[v] = k
+        self.creation_kwargs['aliases'] = aliases
+
+    def map_value(self, instance, value):
+        self._aliases[value]
+        return self._map[self._aliases[value]]
---
+++ tests/features/test_mappings.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+    tests.features.test_mappings
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Module dedicated to testing the mappings features.
+
+    :copyright: 2015 by Lantz Authors, see AUTHORS for more details.
+    :license: BSD, see LICENSE for more details.
+
+"""
+from __future__ import (division, unicode_literals, print_function,
+                        absolute_import)
+
+from lantz_core.features.mappings import Mapping, Bool
+
+
+def test_mapping():
+    m = Mapping(mapping={'On': 1, 'Off': 2})
+    assert m.post_get(None, 1) == 'On'
+    assert m.post_get(None, 2) == 'Off'
+
+    assert m.pre_set(None, 'On') == 1
+    assert m.pre_set(None, 'Off') == 2
+
+
+def test_bool():
+    b = Bool(mapping={True: 1, False: 2},
+             aliases={True: ['On', 'on', 'ON'], False: ['Off', 'off', 'OFF']})
+    assert b.pre_set(None, 'ON') == 1
+    assert b.pre_set(None, 'off') == 2
