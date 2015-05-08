6d68f6cc3396237033c2bf1eb69c4c1c37b5451a
mdartiailh <m.dartiailh@gmail.com>
2015-05-08 23:13:38 +0200

    Implement values, limits validation in Action and checks.
    This adds a dependency to funcsigs which is a backport of inspect.Signature to python lower than 3.3. It is a pure python package .
    
    Checks does not rely anymore on the presence of {} to identify driver related attribute those can be simply accessed through self (which means that one can access to parent attribute which was not possible before). The drawback is that error message is less informative I plan to fix (see comment in code).

--- lantz_core/action.py
+++ lantz_core/action.py
@@ -12,39 +12,45 @@
 from __future__ import (division, unicode_literals, print_function,
                         absolute_import)
 
-from functools import update_wrapper
+from past.builtins import basestring
+from functools import update_wrapper, partial
 from types import MethodType
 
-from .unit import UNIT_SUPPORT, get_unit_registry
+from funcsigs import signature
 
-# XXXX implements checks, values, limits
+from .limits import IntLimitsValidator, FloatLimitsValidator
+from .unit import UNIT_SUPPORT, get_unit_registry
+from .util import (build_checker, validate_in, validate_limits,
+                   get_limits_and_validate)
+
+
 class Action(object):
     """Wraps a method with pre and post processing operations.
 
-    By default only unit processing is supported. Subclass this object to
-    customize the behavior.
+    All parameters must be passed as keyword arguments.
 
     All public driver methods should be decorated as an Action to make them
     easy to identify and hence make instrospection easier.
 
-    Paremeters
+    Parameters
     ----------
-    units : tuple
+    units : tuple, optional
         Tuple of length 2 containing the return unit and the unit of each
         passed argument. None can be used to mark that an argument should not
         be converted. The first argument (self) should always be marked this
         way.
 
-    checks : unicode
-        Booelan tests to execute before anything else. Multiple assertion can
-        be separated with ';'. The driver can be accessed using the following
-        syntax : {driver}. The arguments can be directly accessed using their
-        name.
+    checks : unicode, optional
+        Booelan tests to execute before calling the function. Multiple
+        assertions can be separated with ';'. All the methods arument are
+        available in the assertion execution namespace so one can access to the
+        driver using self and to the arguments using their name (the signature
+        of the wrapper is made to match the signature of the wrapped method).
 
-    values : dict
+    values : dict, optional
         Dictionary mapping the arguments names to their allowed values.
 
-    limits : dict
+    limits : dict, optional
         Dictionary mapping the arguments names to their allowed limits. Limits
         can a be a tuple of length 2, or 3 (min, max, step) or the name of
         the limits to use to check the input.
@@ -52,18 +58,18 @@ class Action(object):
     Notes
     -----
     A single argument should be value checked or limit checked but not both,
-    unit conversion is performed before anything else.
+    unit conversion is performed before anything else. When limit validating
+    against a driver limits the parameter should ALWAYS be converted to the
+    same unit as the one used by the limits.
 
     """
-    __slots__ = ('func', 'kwargs', 'driver')
-
     def __init__(self, **kwargs):
 
         self.kwargs = kwargs
-        self.driver = None
 
     def __call__(self, func):
         update_wrapper(self, func)
+        self.sig = signature(func)
         self.func = self.decorate(func, self.kwargs)
         return self
 
@@ -78,8 +84,17 @@ class Action(object):
         Override this function to alter how
 
         """
+        if 'limits' in kwargs or 'values' in kwargs:
+            func = self.add_values_limits_validation(func,
+                                                     kwargs.get('values', {}),
+                                                     kwargs.get('limits', {})
+                                                     )
+
+        if 'checks' in kwargs:
+            func = self.add_checks(func, kwargs['checks'])
+
         if UNIT_SUPPORT and 'units' in kwargs:
-            return self.add_unit_support(func, kwargs['units'])
+            func = self.add_unit_support(func, kwargs['units'])
 
         return func
 
@@ -89,3 +104,93 @@ class Action(object):
         """
         ureg = get_unit_registry()
         return ureg.wraps(*units, strict=False)(func)
+
+    def add_checks(self, func, checks):
+        """Build a checker function and use it to decorate func.
+
+        Parameters
+        ----------
+        func : callable
+            Function to decorate.
+
+        checks : unicode
+            ; separated string of expression to assert.
+
+        Returns
+        -------
+        wrapped : callable
+            Function wrapped with the assertion checks.
+
+        """
+        check = build_checker(checks, self.sig)
+
+        def check_wrapper(*args, **kwargs):
+            check(*args, **kwargs)
+            return func(*args, **kwargs)
+        update_wrapper(check_wrapper, func)
+        return check_wrapper
+
+    def add_values_limits_validation(self, func, values, limits):
+        """Add arguments validation to call.
+
+        Parameters
+        ----------
+        func : callable
+            Function to decorate.
+
+        values : dict
+            Dictionary mapping the parameters name to the set of allowed
+            values.
+
+        limits : dict
+            Dictionary mapping the parameters name to the limits they must
+            abide by.
+
+        units : dict
+            Dictionary mapping
+
+        Returns
+        -------
+        wrapped : callable
+            Function wrapped with the parameters validation.
+
+        """
+        validators = {}
+        for name, vals in values.items():
+            validators[name] = partial(validate_in, name=name,
+                                       values=set(vals))
+
+        for name, lims in limits.items():
+            if name in validators:
+                msg = 'Arg %s can be limits or values validated not both'
+                raise ValueError(msg % name)
+            if isinstance(lims, (list, tuple)):
+                if any([isinstance(e, float) for e in lims]):
+                    l = FloatLimitsValidator(*lims)
+                else:
+                    l = IntLimitsValidator(*lims)
+
+                validators[name] = partial(validate_limits, limits=l,
+                                           name=name)
+
+            elif isinstance(lims, basestring):
+                validators[name] = partial(get_limits_and_validate,
+                                           limits=lims, name=name)
+
+            else:
+                msg = 'Invalid type for limits values (key {}) : {}'
+                raise TypeError(msg.format(name, type(lims)))
+
+        sig = self.sig
+
+        def wrapper(*args, **kwargs):
+
+            bound = sig.bind(*args, **kwargs).arguments
+            driver = args[0]
+            for n in validators:
+                validators[n](driver, bound[n])
+
+            return func(*args, **kwargs)
+
+        update_wrapper(wrapper, func)
+        return wrapper
--- tests/test_action.py
+++ tests/test_action.py
@@ -11,15 +11,17 @@
 """
 from __future__ import (division, unicode_literals, print_function,
                         absolute_import)
-from pytest import mark
+
+from pytest import mark, raises
 
 from lantz_core.action import Action
+from lantz_core.limits import IntLimitsValidator
 from lantz_core.unit import UNIT_SUPPORT, get_unit_registry
 from .testing_tools import DummyParent
 
 
-def test_action_without_unit():
-    """Test defining an action not using units conversions.
+def test_naked_action():
+    """Test defining a simple action.
 
     """
     class Dummy(DummyParent):
@@ -32,6 +34,98 @@ def test_action_without_unit():
 
     dummy = Dummy()
     assert dummy.test() is Dummy
+
+
+def test_values_action():
+    """Test defining an action with values validation.
+
+    """
+    class Dummy(DummyParent):
+
+        @Action(values={'a': (1, 2, 3)})
+        def test(self, a, b):
+            return a * b
+
+    dummy = Dummy()
+    assert dummy.test(1, 5) == 5
+    with raises(ValueError):
+        dummy.test(5, 2)
+
+
+def test_limits_action1():
+    """Test defining an action with integer limits validation.
+
+    """
+    class Dummy(DummyParent):
+
+        @Action(limits={'b': (1, 10, 2)})
+        def test(self, a, b):
+            return a
+
+    dummy = Dummy()
+    assert dummy.test(1, 1) == 1
+    with raises(ValueError):
+        dummy.test(2,  2)
+
+
+def test_limits_action2():
+    """Test defining an action with floating limits validation.
+
+    """
+    class Dummy(DummyParent):
+
+        @Action(limits={'b': (1.0, 10.0, 0.1)})
+        def test(self, a, b):
+            return a
+
+    dummy = Dummy()
+    assert dummy.test(1, 1)
+    with raises(ValueError):
+        dummy.test(2,  1.05)
+
+
+def test_limits_action3():
+    """Test defining an action getting the limits from the driver.
+
+    """
+    class Dummy(DummyParent):
+
+        @Action(limits={'b': 'c'})
+        def test(self, a, b):
+            return a
+
+        def _limits_c(self):
+            return IntLimitsValidator(1, 10, 2)
+
+    dummy = Dummy()
+    assert dummy.test(1, 1)
+    with raises(ValueError):
+        dummy.test(2,  2)
+
+
+def test_limits_action4():
+    """Test defining an action with the wrong type of limits.
+
+    """
+    with raises(TypeError):
+        class Dummy(DummyParent):
+
+            @Action(limits={'b': 1})
+            def test(self, a, b):
+                return a
+
+
+def test_action_with_overlapping_limits_and_values():
+    """Test defining an action validating the same parameter using values and
+    limits.
+
+    """
+    with raises(ValueError):
+        class Dummy(DummyParent):
+
+            @Action(limits={'b': (1, 10, 2)}, values={'b': (1, 2, 3)})
+            def test(self, a, b):
+                return a
 
 
 @mark.skipif(UNIT_SUPPORT is False, reason="Requires Pint")
