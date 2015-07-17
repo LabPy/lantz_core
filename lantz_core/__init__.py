dced8fefa36f8205dbe4d37cb8260d9e3063f040
Matthieu Dartiailh <marul@laposte.net>
2015-07-17 13:49:34 +0200

    Add some imports to __init__.py

--- lantz_core/__init__.py
+++ lantz_core/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-    lantz
-    ~~~~~
+    lantz_core
+    ~~~~~~~~~~
 
     An automation and instrumentation toolkit with a clean, well-designed and
     consistent interface.
@@ -9,3 +9,15 @@
     :copyright: 2015 by The Lantz Authors
     :license: BSD, see LICENSE for more details.
 """
+from __future__ import (division, unicode_literals, print_function,
+                        absolute_import)
+
+from .has_features import subsystem, channel, set_feat, set_action
+from .action import Action
+from .errors import LantzError
+from .limits import IntLimitsValidator, FloatLimitsValidator
+from .unit import set_unit_registry, get_unit_registry
+
+__all__ = ['subsystem', 'channel', 'set_action', 'set_feat', 'Action',
+           'LantzError', 'set_unit_registry', 'get_unit_registry',
+           'IntLimitsValidator', 'FloatLimitsValidator']
